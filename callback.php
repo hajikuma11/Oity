09f1c65a5362afeb88bfc43cada98c96e600a752
BuildTools <unconfigured@null.spigotmc.org>
2018-05-01 00:29:08 +0900

    make it better

--- callback.php
+++ callback.php
@@ -9,49 +9,18 @@ $jsonObj = json_decode($jsonString);
 $message = $jsonObj->{"events"}[0]->{"message"};
 $replyToken = $jsonObj->{"events"}[0]->{"replyToken"};
 
+// 送られてきたメッセージの中身からレスポンスのタイプを選択
 if ($message->{"text"} == '天気') {
-    
-    require_once('weather.php');
-    
-    $time = new DateTime('now', new DateTimeZone('Asia/Tokyo'));
-    
-    $weather  = new Weather('osaka');
-    $now    = $weather->GetCondition();
-    $today   = $weather->GetToday();
-    
-    $messageData = [
+     $messageData = [
         'type' => 'text',
-        'text' =>  $message  = '('
-           $time->format('m/d H:i')
-          ')'
-           PHP_EOL
-           ' 現在における天気は'
-           PHP_EOL
-          '  '
-           $now['weather']
-           '('
-           $now['temp']
-           '℃)'
-           PHP_EOL
-           'です。'
-           PHP_EOL
-           '今日の天気は'
-           PHP_EOL
-          '  '
-           $today["weather"]
-           'で,'
-           PHP_EOL
-          ' 　 '
-           '最高気温は'
-           $today['high']
-           '℃, 最低気温は'
-           $today['low']
-           '℃'
-           PHP_EOL
-          'でしょう。';
-        
+        'text' => $message->{"あああ"}
     ];
 }
+
+$response = [
+    'replyToken' => $replyToken,
+    'messages' => [$messageData]
+];
 error_log(json_encode($response));
 
 $ch = curl_init('https://api.line.me/v2/bot/message/reply');
