ac99e2c6067c3a2b02147aaf4ce1ff04fb59461b
BuildTools <unconfigured@null.spigotmc.org>
2018-05-01 00:37:13 +0900

    make it better

--- callback.php
+++ callback.php
@@ -1,6 +1,84 @@
 <?php
 
-require_once('weather.php');
+class Weather{
+    private $city;
+    private $info;
+ 
+    public function __construct($city){ //コンストラクタ
+        $this->city = $city;
+        $this->info = $this->GetWeather();
+    }
+ 
+    private function FtoC($f){ // 華氏→摂氏変換
+        return round(($f - 32) * 0.555, 1);
+    }
+ 
+    private function Translation($weather){ //翻訳
+        switch ($weather) {
+            case 'Sunny':
+            case 'Clear':
+            case 'Mostly Sunny':
+            case 'Mostly Clear':
+                $weather=" 晴れ ";
+                break;
+            case 'Partly Cloudy':
+            case 'Mostly Cloudy':
+                $weather=" 晴れときどき曇り ";
+                break;
+            case 'Cloudy':
+                $weather=" 曇り ";
+                break;
+            case 'Breezy':
+                $weather=" そよ風 ";
+                break;
+            case 'Windy':
+                $weather=" 風が強い ";
+                break;
+            case 'Scattered Showers':
+            case 'Showers':
+            case 'Rain':
+            case 'Thunderstorms':
+            case 'Scattered Thunderstorms':
+                $weather=" 雨 ";
+                break;
+        }
+        return $weather;
+    }
+ 
+    private function GetWeather(){ //天気情報取得
+        return json_decode(file_get_contents(
+            'https://query.yahooapis.com/v1/public/yql?q=select%09*%20from%20%09weather.forecast%20%20where%20%09woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22'
+            . $this->city
+            . '%2C%20jp%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys'
+            ))->query->results->channel->item;
+    }
+ 
+    public function GetCondition(){ //現在の天気
+        $now = [
+            'weather' => $this->Translation($this->info->condition->text)
+            , 'temp'  => $this->FtoC($this->info->condition->temp)
+        ];
+        return $now;
+    }
+ 
+    public function GetToday(){ //今日の天気
+        $today = [
+            'weather' => $this->Translation($this->info->forecast[0]->text)
+            , 'high'  => $this->FtoC($this->info->forecast[0]->high)
+            , 'low'   => $this->FtoC($this->info->forecast[0]->low)
+        ];
+        return $today;
+    }
+ 
+    public function GetTomorrow(){ //明日の天気
+        $tomorrow = [
+            'weather' => $this->Translation($this->info->forecast[1]->text)
+            , 'high'  => $this->FtoC($this->info->forecast[1]->high)
+            , 'low'   => $this->FtoC($this->info->forecast[1]->low)
+        ];
+        return $tomorrow;
+    }
+}
 
 $accessToken = 'HjUjwJORNXxUyK/BJ3zw5+IVAnZ9lOcUHkgTxN7FGECcmS3jnIAndMcuUfW5qpazytxUVR62hXsqpv00JeXU9kjw9WLqesWYATfEmXabOoEt/FeYJPk2d4UJstPKwrlvRfdRHVpiucEX3K1n17qYDAdB04t89/1O/w1cDnyilFU=';
 
