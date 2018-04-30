d0cb4e6b89eaa06e952b54f6776fedae20f67265
BuildTools <unconfigured@null.spigotmc.org>
2018-04-30 23:05:06 +0900

    make it better

--- callback.php
+++ callback.php
@@ -1,6 +1,6 @@
 <?php
 
-$accessToken = 'Line Developersで発行したアクセストークン';
+$accessToken = 'HjUjwJORNXxUyK/BJ3zw5+IVAnZ9lOcUHkgTxN7FGECcmS3jnIAndMcuUfW5qpazytxUVR62hXsqpv00JeXU9kjw9WLqesWYATfEmXabOoEt/FeYJPk2d4UJstPKwrlvRfdRHVpiucEX3K1n17qYDAdB04t89/1O/w1cDnyilFU=';
 
 $jsonString = file_get_contents('php://input');
 error_log($jsonString);
