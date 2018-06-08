aa492d47119af666f3958935876efcf0d3cfe26e
元 松井 <hajimekuma11@gmail.com>
2018-06-08 06:44:55 +0000

    Pushed from Dropbox

--- decimal.php
+++ decimal.php
@@ -1,9 +1,48 @@
 <?php
-$TDATA = substr($text,6);
-$sec =decbin($TDATA);
-$eig =octdec($TDATA);
-$sixt =dechex($TDATA);
-$messageData = [
+
+$strstrtext = mb_strstr($text,'数');
+$s_text = substr($strstrtext,3);
+
+if (strstr($text,'2進数')) {//２進数から
+  $ten =bindec($s_text);
+  $eig =decoct($ten);
+  $sixt =dechex($ten);
+
+  $messageData = [
+   'type' => 'text',
+   'text' => "[8]$eig\n[10]$ten\n[16]$sixt"
+  ];
+};
+
+elseif (strstr($text,'8進数')) {//８進数から
+  $ten =octdec($s_text);
+  $sec =decbin($ten);
+  $sixt =dechex($ten);
+
+  $messageData = [
+   'type' => 'text',
+   'text' => "[2]$sec\n[10]$ten\n[16]$sixt"
+  ];
+};
+
+elseif (strstr($text,'16進数')) {//１６進数から
+  $ten =hexdec($s_text);
+  $sec =decbin($ten);
+  $eig =decoct($ten);
+
+  $messageData = [
+   'type' => 'text',
+   'text' => "[2]$sec\n[8]$eig\n[10]$ten"
+  ];
+};
+
+else {//１０進数から
+  $sec =decbin($s_text);
+  $eig =decoct($s_text);
+  $sixt =dechex($s_text);
+
+  $messageData = [
    'type' => 'text',
    'text' => "[2]$sec\n[8]$eig\n[16]$sixt"
-];
+  ];
+};
