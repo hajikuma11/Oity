1f505e7b4e2bb4644f0136741f8bff99f2f5da8e
元 松井 <hajimekuma11@gmail.com>
2018-06-22 05:28:09 +0000

    Pushed from Dropbox

--- help.php
+++ help.php
@@ -59,27 +59,3 @@ $messageData = [
    'text' => $msg
 ];
 }
-
-elseif ($text == 'flex') {
-  $messageData = [
-    'type' => 'flex',
-  'altText' => 'this is a flex message',
-  'contents' = [
-    'type' => 'bubble',
-    'body' => [
-      'type' => 'box',
-      'layout' => 'vertical',
-      'contents' => [
-        [
-          'type' => 'text',
-          'text' => 'hello'
-        ],
-        [
-          'type' => 'text',
-          'text' => 'world'
-        ],
-      ]
-    ]
-  ]
-];
-}
