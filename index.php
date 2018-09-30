fc6d0ca171610197260b98d6b6932fcf4ad2856e
hajikuma11 <40023230+hajikuma11@users.noreply.github.com>
2018-09-30 21:50:34 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
-<?php
-echo "hello,user.";
- ?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>新Oityのご案内</title>
+    </head>
+<body>
+    <a href="https://line.me/R/ti/p/%40xai0109a"><img height="36" border="0" alt="友だち追加" src="https://scdn.line-apps.com/n/line_add_friends/btn/ja.png"></a>
+</body>
+</html>
