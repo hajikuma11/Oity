009c21624c2191751692aca216276ab874d7962f
hajikuma11 <40023230+hajikuma11@users.noreply.github.com>
2018-10-03 16:27:30 +0900

    Update Wolf.php

--- Wolf.php
+++ Wolf.php
@@ -1,61 +1,61 @@
 <?php
 switch ($text) {
 case '人狼':
-    $messageData = [
-      'type' => 'text',
-      'text' => '遊ぶ人数を選択してください',
-      'quickReply' => [
+$messageData = [
+    'type' => 'text',
+    'text' => '何人であそぶ？',
+    'quickReply' => [
         'items' => [
-                    [
-                      'type' => 'action',
-                      'action' => [
-                        'type' => 'text',
-                        'label' => '4人',
-                        'text' => 'wwolf4'
-                     ]
-                    ],
-                    [
-                      'type' => 'action',
-                      'action' => [
-                        'type' => 'text',
-                        'label' => '5人',
-                        'text' => 'wwolf5'
-                     ]
-                    ],
-                    [
-                      'type' => 'action',
-                      'action' => [
-                        'type' => 'text',
-                        'label' => '6人',
-                        'text' => 'wwolf6'
-                     ]
-                    ],
-                    [
-                      'type' => 'action',
-                      'action' => [
-                        'type' => 'text',
-                        'label' => '7人',
-                        'text' => 'wwolf7'
-                     ]
-                    ],
-                    [
-                      'type' => 'action',
-                      'action' => [
-                        'type' => 'text',
-                        'label' => '8人',
-                        'text' => 'wwolf8'
-                     ]
-                    ],
-                    [
-                      'type' => 'action',
-                      'action' => [
-                        'type' => 'text',
-                        'label' => '9人',
-                        'text' => 'wwolf9'
-                     ]
-                    ]
-                  ]
+            [
+            'type' => 'action',
+            'action' => [
+              'type' => 'message',
+              'label' => '4人',
+              'text' => 'wwf4'
                 ]
-              ];
+            ],
+            [
+            'type' => 'action',
+            'action' => [
+              'type' => 'message',
+              'label' => '5人',
+              'text' => 'wwf5'
+                ]
+            ],
+            [
+            'type' => 'action',
+            'action' => [
+              'type' => 'message',
+              'label' => '6人',
+              'text' => 'wwf6'
+                ]
+            ],
+            [
+            'type' => 'action',
+            'action' => [
+              'type' => 'message',
+              'label' => '7人',
+              'text' => 'wwf7'
+                ]
+            ],
+            [
+            'type' => 'action',
+            'action' => [
+              'type' => 'message',
+              'label' => '8人',
+              'text' => 'wwf8'
+                ]
+            ],
+            [
+            'type' => 'action',
+            'action' => [
+              'type' => 'message',
+              'label' => '9人',
+              'text' => 'wwf9'
+                ]
+            ]
+        ]
+    ]
+];
 break;
 }
