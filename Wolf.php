9b93b8ea0775cb764bddadcf57a9108fae56024c
hajikuma11 <40023230+hajikuma11@users.noreply.github.com>
2018-10-03 16:00:17 +0900

    Add files via upload

---
+++ Wolf.php
@@ -0,0 +1,31 @@
+<?php
+switch ($text) {
+case '人狼':
+    $messageData = [
+      'type' => 'text',
+      'text' => '遊ぶ人数を選択してください',
+      'quickReply' -> [
+        'items' -> [
+                    [
+                      'type' -> 'action',
+                      'action' -> [
+                        'type' -> 'postback',
+                        'label' -> '4人',
+                        'data' -> 'wwold4'
+                     ]
+                    ]
+                    for ($fig=5:$fig<10:$fig++) {
+                      ,[
+                        'type' -> 'action',
+                        'action' -> [
+                          'type' -> 'postback',
+                          'label' -> $fig.'人',
+                          'data' -> 'wwold'.$fig
+                       ]
+                      ]
+                    }
+                  ]
+                ]
+              ];
+break;
+}
