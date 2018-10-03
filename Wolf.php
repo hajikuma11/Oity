00a961753b9910cfd84344960fdcc50e8b1be4e5
hajikuma11 <40023230+hajikuma11@users.noreply.github.com>
2018-10-03 16:13:47 +0900

    Update Wolf.php

--- Wolf.php
+++ Wolf.php
@@ -4,28 +4,28 @@ case '人狼':
     $messageData = [
       'type' => 'text',
       'text' => '遊ぶ人数を選択してください',
-      'quickReply' -> {
-        'items' -> {
-                     {
-                      'type' -> 'action',
-                      'action' -> {
-                        'type' -> 'postback',
-                        'label' -> '4人',
-                        'data' -> 'wwold4'
-                      }
-                     }
+      'quickReply' => [
+        'items' => [
+                    [
+                      'type' => 'action',
+                      'action' => [
+                        'type' => 'postback',
+                        'label' => '4人',
+                        'data' => 'wwold4'
+                     ]
+                    ]
                     for ($fig=5:$fig<10:$fig++) {
-                      ,{
-                        'type' -> 'action',
-                        'action' -> {
-                          'type' -> 'postback',
-                          'label' -> $fig.'人',
-                          'data' -> 'wwold'.$fig
-                            }
-                      }
+                      ,[
+                        'type' => 'action',
+                        'action' => [
+                          'type' => 'postback',
+                          'label' => $fig.'人',
+                          'data' => 'wwold'.$fig
+                       ]
+                      ]
                     }
-                  }
-                }
+                  ]
+                ]
               ];
 break;
 }
