1ff31d8e5cc3a5f26fcc157af0c4d8324fe65b37
hajikuma11 <40023230+hajikuma11@users.noreply.github.com>
2018-10-26 14:24:50 +0900

    Delete AcessToken.php

--- AcessToken.php
+++
@@ -1,2 +0,0 @@
-<?php
-$acessToken = 'HjUjwJORNXxUyK/BJ3zw5+IVAnZ9lOcUHkgTxN7FGECcmS3jnIAndMcuUfW5qpazytxUVR62hXsqpv00JeXU9kjw9WLqesWYATfEmXabOoEt/FeYJPk2d4UJstPKwrlvRfdRHVpiucEX3K1n17qYDAdB04t89/1O/w1cDnyilFU=';
