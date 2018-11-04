b39e45b36e3d6232834a42ac571b30702617249b
hajikuma11 <hajimekuma11@gmail.com>
2018-11-04 23:07:45 +0900

    Update callback.php

--- callback.php
+++ callback.php
@@ -184,7 +184,7 @@ elseif ($text == 'pg') {
 $dsn = sprintf('pgsql:host=%s;dbname=%s', $url['host'], substr($url['path'], 1));
 
 $pdo = new PDO($dsn, $url['user'], $url['pass']);
-$testTxt == $pdo->getAttribute(PDO::ATTR_SERVER_VERSION);
+$testTxt = $pdo->getAttribute(PDO::ATTR_SERVER_VERSION);
 
 $messageData = [
   'type' => 'text',
