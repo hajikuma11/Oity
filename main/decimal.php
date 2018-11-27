dacecf9870ade28ed265e548c3e30b0ded5d6dd7
hajikuma11 <hajimekuma11@gmail.com>
2018-11-27 11:04:31 +0900

    else文に格納されていなかったのを修正

--- main/decimal.php
+++ main/decimal.php
@@ -8,52 +8,52 @@ if ($s_text >= 1) {
      'type' => 'text',
      'text' => "エラー\n1以上の小数点形式には対応していません。\n'2'or'10'進数0.'～～'\nのように入力してください"
     ];
+  } else {
+    if (strstr($text,'2進数')) {//２進数から
+      $ten =bindec($s_text);
+      $eig =decoct($ten);
+      $sixt =dechex($ten);
+
+      $messageData = [
+       'type' => 'text',
+       'text' => "[8]$eig\n[10]$ten\n[16]$sixt"
+      ];
+    }
+
+    elseif (strstr($text,'8進数')) {//８進数から
+      $ten =octdec($s_text);
+      $sec =decbin($ten);
+      $sixt =dechex($ten);
+
+      $messageData = [
+       'type' => 'text',
+       'text' => "[2]$sec\n[10]$ten\n[16]$sixt"
+      ];
+    }
+
+    elseif (strstr($text,'16進数')) {//１６進数から
+      $ten =hexdec($s_text);
+      $sec =decbin($ten);
+      $eig =decoct($ten);
+
+      $messageData = [
+       'type' => 'text',
+       'text' => "[2]$sec\n[8]$eig\n[10]$ten"
+      ];
+    }
+
+    elseif (strstr($text,'進数') or strstr($text,'10進数')) {//１０進数から
+      $sec =decbin($s_text);
+      $eig =decoct($s_text);
+      $sixt =dechex($s_text);
+
+      $messageData = [
+       'type' => 'text',
+       'text' => "[2]$sec\n[8]$eig\n[16]$sixt"
+      ];
+    }
+
   }
-
-  if (strstr($text,'2進数')) {//２進数から
-    $ten =bindec($s_text);
-    $eig =decoct($ten);
-    $sixt =dechex($ten);
-
-    $messageData = [
-     'type' => 'text',
-     'text' => "[8]$eig\n[10]$ten\n[16]$sixt"
-    ];
-  }
-
-  elseif (strstr($text,'8進数')) {//８進数から
-    $ten =octdec($s_text);
-    $sec =decbin($ten);
-    $sixt =dechex($ten);
-
-    $messageData = [
-     'type' => 'text',
-     'text' => "[2]$sec\n[10]$ten\n[16]$sixt"
-    ];
-  }
-
-  elseif (strstr($text,'16進数')) {//１６進数から
-    $ten =hexdec($s_text);
-    $sec =decbin($ten);
-    $eig =decoct($ten);
-
-    $messageData = [
-     'type' => 'text',
-     'text' => "[2]$sec\n[8]$eig\n[10]$ten"
-    ];
-  }
-
-  elseif (strstr($text,'進数') or strstr($text,'10進数')) {//１０進数から
-    $sec =decbin($s_text);
-    $eig =decoct($s_text);
-    $sixt =dechex($s_text);
-
-    $messageData = [
-     'type' => 'text',
-     'text' => "[2]$sec\n[8]$eig\n[16]$sixt"
-    ];
-  }
-
 } else {
 
 $data = $s_text;
