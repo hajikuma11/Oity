8e3866e501ad2134113aa79cb858f813a1865512
hajikuma11 <hajimekuma11@gmail.com>
2018-12-18 14:38:25 +0900

    Update rename.php

--- main/rename.php
+++ main/rename.php
@@ -4,6 +4,6 @@ $txt = file_get_contents('123.txt');
 $txt = str_replace('{','[',$txt);
 $txt = str_replace('}',']',$txt);
 $txt = str_replace('"',"'",$txt);
-$txt = str_replace(':',' =>',$txt);
+$txt = str_replace("':","' =>",$txt);
 
 file_put_contents('123.txt',$txt);
