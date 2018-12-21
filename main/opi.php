54685c491e58418e6ead8f06cdf45185caa0bea0
hajikuma11 <hajimekuma11@gmail.com>
2018-12-21 15:07:23 +0900

    opi

---
+++ main/opi.php
@@ -0,0 +1,54 @@
+<?php
+$messsageData = [
+    'type' => 'flex',
+    'altText' => 'flexmessage',
+    'contents' => [
+        'type' => 'bubble',
+        'styles' => [
+            'footer' => [
+                'separator' => true
+            ]
+        ],
+        'body' => [
+            'type' => 'box',
+            'layout' => 'vertical',
+            'contents' => [
+                [
+                    'type' => 'text',
+                    'text' => 'OPPAI Function',
+                    'weight' => 'bold',
+                    'color' => '#1DB446',
+                    'size' => 'sm'
+                ],
+                [
+                    'type' => 'text',
+                    'text' => 'お',
+                    'weight' => 'bold',
+                    'size' => 'xxl',
+                    'margin' => 'md'
+                ],
+                [
+                    'type' => 'text',
+                    'text' => '　 　っ',
+                    'weight' => 'bold',
+                    'size' => 'xxl',
+                    'margin' => 'md'
+                ],
+                [
+                    'type' => 'text',
+                    'text' => '　　　　　ぱ',
+                    'weight' => 'bold',
+                    'size' => 'xxl',
+                    'margin' => 'md'
+                ],
+                [
+                    'type' => 'text',
+                    'text' => '　　　　　　　い',
+                    'weight' => 'bold',
+                    'size' => 'xxl',
+                    'margin' => 'md'
+                ]
+            ]
+        ]
+    ]
+];
