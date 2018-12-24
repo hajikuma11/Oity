3ebde3f8d29923fc3ffeea2014fcc113f0f02ce3
hajikuma11 <hajimekuma11@gmail.com>
2018-12-24 12:05:41 +0900

    Update opi.php

--- main/opi.php
+++ main/opi.php
@@ -1,5 +1,6 @@
 <?php
 $json = file_get_contents(__DIR__.'opi.json');
+mb_language('Japanese');
 $json = mb_convert_encoding($json, 'UTF8', 'ASCII,JIS,UTF-8,EUC-JP,SJIS-WIN');
 $arr = json_decode($json,true);
 
