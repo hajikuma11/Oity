433670a4a4360cf8ec689b21c18e230a39351df9
hajikuma11 <hajimekuma11@gmail.com>
2018-12-24 16:17:00 +0900

    Update opi.php

--- main/opi.php
+++ main/opi.php
@@ -1,12 +1,10 @@
 <?php
-$txt = file_get_contents(__DIR__.'opi.json');
-$txt = str_replace('{','[',$txt);
-$txt = str_replace('}',']',$txt);
-$txt = str_replace('"',"'",$txt);
-$txt = str_replace("':","' =>",$txt);
+$json = file_get_contents(__DIR__.'/opi.json');
+$json = mb_convert_encoding($json, 'UTF8', 'ASCII,JIS,UTF-8,EUC-JP,SJIS-WIN');
+$arr = json_decode($json,true);
 
 $messsageData = [
     'type' => 'flex',
     'altText' => 'flexmessage',
-    'contents' => $txt
+    'contents' => $arr
 ];
