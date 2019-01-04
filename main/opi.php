dfc27cb398b3918055424b452ab7ac22e7d09648
hajikuma11 <hajimekuma11@gmail.com>
2019-01-04 14:20:03 +0900

    Update opi.php

--- main/opi.php
+++ main/opi.php
@@ -1,7 +1,7 @@
 <?php
 $json = file_get_contents(__DIR__.'/opi.json');
 //$json = json_decode($json,true);
-$json = preg_replace("/( |　|\n)/","",$json);
+$json = preg_replace("/( |　|\n|\)/","",$json);
 
 $messageData = [
     'type' => 'flex',
