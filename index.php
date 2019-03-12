8e53d9e9adddf58b24f9e6624d2b8ba824e25b35
hajikuma11 <40023230+hajikuma11@users.noreply.github.com>
2019-03-12 21:49:50 +0900

    webhookを受け取れるように改良中

--- index.php
+++ index.php
@@ -1,13 +1,4 @@
 <?php
-echo __FILE__;
-?>
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <title>新Oityのご案内</title>
-    </head>
-<body>
-    <a href="https://line.me/R/ti/p/%40xai0109a"><img height="36" border="0" alt="友だち追加" src="https://scdn.line-apps.com/n/line_add_friends/btn/ja.png"></a>
-</body>
-</html>
+$body = file_get_contents('php://input');
+$payload = json_decode($body);
+var_dump($payload);
