262a4122fa6e699a793fcca85b666547a8afd2b9
hajikuma11 <40023230+hajikuma11@users.noreply.github.com>
2019-03-12 21:57:24 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
 $body = file_get_contents('php://input');
-$payload = json_decode($body);
-file_put_contents('log.txt', $payload);
+$payload = json_decode($body,true);
+print var_export($payload , true);
