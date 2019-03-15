df4c3232848ef6d9686072c912a476fcec037b1e
hajikuma11 <40023230+hajikuma11@users.noreply.github.com>
2019-03-15 16:43:50 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,5 @@
 <?php
-$cnt = $_POST->{changes}[0]->{body}[0]->{from}[0];
 $imp = implode("  ",$_POST);
-$yaji = $_POST->{action}[0];
 
 $url = 'https://script.google.com/macros/s/AKfycbw8sFc-jlQrlZJOf_8fvbnrO1KUjsrfwv376piCGXxV6PlV6j03/exec';
-file_get_contents($url."?p1=$_POST"."&p2=".$cnt."&p3=".$imp."&p4=".$yaji);
+file_get_contents($url."?p1=$imp"."?p2=$imp"."?p3=$imp"."?p4=$imp");
