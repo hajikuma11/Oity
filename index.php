3a1e58ea18eb9bad1eea6317efec7c659e28d982
hajikuma11 <40023230+hajikuma11@users.noreply.github.com>
2019-03-15 15:43:08 +0900

    Update index.php

--- index.php
+++ index.php
@@ -2,4 +2,4 @@
 $request = json_decode(file_get_contents('php://input'), true);
 
 $url = 'https://script.google.com/macros/s/AKfycbw8sFc-jlQrlZJOf_8fvbnrO1KUjsrfwv376piCGXxV6PlV6j03/exec';
-file_get_contents($url."?p1=$request");
+file_get_contents($url."?p1=$request[1]");
