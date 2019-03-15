17b8075b226d8d0576754b1685c744372a307c78
hajikuma11 <40023230+hajikuma11@users.noreply.github.com>
2019-03-15 15:59:59 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
 $request = json_decode(file_get_contents('php://input'), true);
-$rec1 = $request[0]
-$rec2 = $request[1]
-$rec3 = $request[2]
-$rec4 = $request[3]
+$rec1 = $request[0];
+$rec2 = $request[1];
+$rec3 = $request[2];
+$rec4 = $request[3];
 $url = 'https://script.google.com/macros/s/AKfycbw8sFc-jlQrlZJOf_8fvbnrO1KUjsrfwv376piCGXxV6PlV6j03/exec';
 file_get_contents($url."?p1=$rec1&p2=$rec2&p3=$rec3&p4=$rec4");
