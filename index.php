4011d165bc3897d3c4b7d4a64c32ce95d10145e9
hajikuma11 <40023230+hajikuma11@users.noreply.github.com>
2019-03-15 22:36:52 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,3 @@
 <?php
-$url = 'https://script.google.com/macros/s/AKfycbw8sFc-jlQrlZJOf_8fvbnrO1KUjsrfwv376piCGXxV6PlV6j03/exec';
+$url = 'https://script.google.com/macros/s/AKfycbyrJAffhej-etQRoYWgGRqvfldC8Vt4u8Kvf13fo9AA_LAuFlYU/exec';
 file_get_contents($url."?p1=テスト");
