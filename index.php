8cb54db349f7f8f92ae592c19dab29e902e35878
hajikuma11 <40023230+hajikuma11@users.noreply.github.com>
2019-03-15 17:09:33 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,7 @@
 <?php
 $imp = implode("",$_POST);
 $imp = explode(",",$imp);
-$str = "?p1=$imp[0]";
-
-for ($i=1;$i<10;$i++) {
-  $pv = $i + 1;
-  $str .= '&p'.$pv.'='.$imp[$i];
-}
+$str = "?p1=$imp[0]"."&p2=$imp[1]"."&p3=$imp[2]"."&p4=$imp[3]"."&p5=$imp[4]"."&p6=$imp[5]"."&p7=$imp[6]"."&p8=$imp[7]"."&p9=$imp[8]"."&p10=$imp[9]";
 
 $url = 'https://script.google.com/macros/s/AKfycbw8sFc-jlQrlZJOf_8fvbnrO1KUjsrfwv376piCGXxV6PlV6j03/exec';
 file_get_contents($url.$str);
