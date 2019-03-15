223c5608b2f85e65c50fa91a5fe2cc0049fcfe6a
hajikuma11 <40023230+hajikuma11@users.noreply.github.com>
2019-03-15 16:45:34 +0900

    Update index.php

--- index.php
+++ index.php
@@ -2,4 +2,4 @@
 $imp = implode("  ",$_POST);
 
 $url = 'https://script.google.com/macros/s/AKfycbw8sFc-jlQrlZJOf_8fvbnrO1KUjsrfwv376piCGXxV6PlV6j03/exec';
-file_get_contents($url."?p1=$imp"."?p2=$imp"."?p3=$imp"."?p4=$imp");
+file_get_contents($url."?p1=$imp");
