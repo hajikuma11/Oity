6522b4d16adfe91f9587a4b26fbd475b6826274d
hajikuma11 <40023230+hajikuma11@users.noreply.github.com>
2019-03-25 16:56:52 +0900

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ $cnt = count($arr);
 for ($i=0;$i<$cnt;$i++) {
   $arr[$i] = str_replace('"','',$arr[$i]);
   if (strlen($arr[$i]) > 25) {
-    $arr[$i] = substr($arr[$i],0,15);
+    $arr[$i] = substr($arr[$i],0,20);
   }
 }
 
